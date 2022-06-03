c5c7176d8f0e190d3541ce0f5c769e65395613dc
Analyze_ <101422486+Analyze0@users.noreply.github.com>
2022-06-03 10:54:37 +0000

    fixed code formatting

--- .ccls-cache/@home@runner@Binary-to-Decimal-Decoder/main.cpp
+++ .ccls-cache/@home@runner@Binary-to-Decimal-Decoder/main.cpp
@@ -4,16 +4,19 @@ using namespace std;
 int main() {
   char binaryNumber[] = "0101010101010101010101010101010101";
   string session = "y";
-  while (session == "y"){
-     
-cout << "\nEnter a Binary number (1s and 0s): "; cin >> binaryNumber; cout << "\n";
-    
-cout << stoi(binaryNumber, 0, 2);
-session = "y";
-cout << "\nContinue session? [y/n]\n";
-   cin >> session;
+  while (session == "y") {
+
+    cout << "\nEnter a Binary number (1s and 0s): ";
+    cin >> binaryNumber;
+    cout << "\n";
+
+    cout << stoi(binaryNumber, 0, 2);
+    session = "y";
+    cout << "\nContinue session? [y/n]\n";
+    cin >> session;
     cout << "\x1B[2J\x1B[H";
-    }//end of session
-  cout << "Credit to https://geeksforgeeks.org/ for helping me with some of this code!\n";
+  } // end of session
+  cout << "Credit to https://geeksforgeeks.org/ for helping me with some of "
+          "this code!\n";
   return 1;
 }
