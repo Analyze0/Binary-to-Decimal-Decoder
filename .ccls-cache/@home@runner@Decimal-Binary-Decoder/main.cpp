2c14d8d359b48320b6dee505f18f32771841a214
Analyze_ <101422486+Analyze0@users.noreply.github.com>
2022-06-03 10:52:42 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Decimal-Binary-Decoder/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  char binaryNumber[] = "0101010101010101010101010101010101";
+  string session = "y";
+  while (session == "y"){
+     
+ 
+cout << "\nEnter a Binary number (1s and 0s): "; cin >> binaryNumber; cout << "\n";
+
+cout << stoi(binaryNumber, 0, 2);
+session = "y";
+cout << "\nContinue session? [y/n]\n";
+   cin >> session;
+    cout << "\x1B[2J\x1B[H";
+    }//end of session
+  return 1;
+}
